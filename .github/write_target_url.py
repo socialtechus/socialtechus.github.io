a06b82eb2cef9e9c037ac4f9e60e79b1e6f3273b
Sergio Sánchez Zavala <chekos.data@gmail.com>
2020-05-07 11:46:05 -0700

    Create write_target_url.py

---
+++ .github/write_target_url.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+with open("test.json") as file:
+    test = json.load(file)
+    
+pr_url = test['pull_request']['url']
+pr_content = json.loads(requests.get(pr_url).content)
+status_url = pr_content['statuses_url']
+latest_status = json.loads(requests.get(status_url).content)[0]
+latest_status['target_url']
+
+with open("target_url.txt", "w") as file:
+    file.write(latest_status['target_url'])
